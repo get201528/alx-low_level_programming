2fb490e546aa7ae9218000b697817f9a67db8746
get201528 <getsun201528@gmail.com>
2022-12-18 00:11:23 -0800

    a cprogramming which pring a string using printf func

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * descriptio - printing a given string using printf
+ *
+ * Return:0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
