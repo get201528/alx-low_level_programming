9a6dcb9bc2cfeac4c436932f475e515376dc7db6
get201528 <getsun201528@gmail.com>
2022-12-17 22:54:34 -0800

    Myfirst c programmint onwebterm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
   *main-prints the passed string using puts function
-  *Retur 0
+  *Return 0
   */
 int main(void)
 {
 	char str1[] = "\"Programming is like building a multilingual puzzle";
+	
 	puts(str1);
+
 	return (0);
 }
