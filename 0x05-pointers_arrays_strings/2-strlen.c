1ba0dc19a8289ddcad6ace8b5dee2bdccd2d6cd2
get201528 <getsun201528@gmail.com>
2022-12-19 15:54:31 -0800

    Task 2 a function print the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * _strlen - return length of the string
+ * @s: string
+ * Return: lenngth
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+}
