96f8a3f2ece07edf177536aa2712a87d46ff071c
get201528 <getsun201528@gmail.com>
2022-12-21 00:29:12 -0800

    Task 0 a function that print if a function is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n == 0)
-		printf("is zero\n");
+		printf("%n is zero\n", n);
 	else if (n < 0)
-		printf("is negative\n");
+		printf("%n is negative\n", n);
 	else
-		printf("is positive\n");
+		printf("% is positive\n", n);
 	return (0);
 }
