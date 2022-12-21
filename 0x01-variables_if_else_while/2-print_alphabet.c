3568c3917f6aa1badd548bb4783c14ae1152b88f
get201528 <getsun201528@gmail.com>
2022-12-21 07:03:35 -0800

    Task 3 to upper case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <ctype.h>
+/**
+ * main - a function change to uppercae
+ * Return: 0
+ */
 int main(void)
 {
 	char ch;
 
 	int putchar(int ch)
-		scanf(ch);
-	puchar(toupper(ch));
+        ch = scanf();
+	putchar(toupper(ch));
 	return (0);
 }
