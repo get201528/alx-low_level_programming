a635be3c1e2a01d78e4358ae7049b786b91bdc6c
get201528 <getsun201528@gmail.com>
2022-12-21 20:47:41 -0800

    Task 8 base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print all number of small letter in base 16
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int d;
+
+	while (d < 10)
+	{
+		putchar(d + '0');
+		d++;
+	}
+	while (c <= 'f')
+	{
+		putchar (c);
+		c++
+	}
+	putchar('\n');
+	return (0);
+}
