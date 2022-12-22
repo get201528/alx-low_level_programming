762a00a8fd70340165d13269b7eaf2536c540106
get201528 <getsun201528@gmail.com>
2022-12-21 22:47:27 -0800

    Task 4 printing except e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 /**
- * main - print the alaphapet except e and
+ * main - print the alaphapet except e and q
  * Return: 0
  */
 int main(void)
 {
-	char ch = 'a';
+	char ch;
 
+	ch = 'a'
 	while (ch <=  'z')
 	{
-		if (ch != 'e' && ch != 'q')
-		{
-		putchar(ch);
-		}
+		if ((ch != 'e' && ch != 'q') && ch <= 'z')
+			putchar(ch);
 		ch++;
 	}
 	putchar('\n');
