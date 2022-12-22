b4c11c8d2975490987a77d2b0c7b2382102700fa
get201528 <getsun201528@gmail.com>
2022-12-21 20:05:04 -0800

    Task 6 printing 0-9 using putcahr

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - printing 0 - 9 using putchar
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar("\n");
+	return (0);
+}
