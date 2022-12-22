abbcc77f729a309053cc5682f48b004531365469
get201528 <getsun201528@gmail.com>
2022-12-21 19:49:36 -0800

    Task 5 Printing from 0_9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints all single numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
