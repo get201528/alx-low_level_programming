1b93a20087309f5a22bd05d92046007096b24e51
get201528 <getsun201528@gmail.com>
2022-12-22 16:53:53 -0800

    Task 2 copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _stncpy - copy string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+char *_stncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[i];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\n';
+		j++
+	}
+	return (dest);
+}
