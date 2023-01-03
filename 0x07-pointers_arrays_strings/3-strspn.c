a2842d3c4fe51523c396d57056d827bce52f6113
get201528 <getsun201528@gmail.com>
2023-01-03 15:31:11 -0800

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,20 +10,20 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int bytes = 0;
 	int index;
-	
+
 	while (*s)
 	{
 		for (index = 0;  accept[index]; index++)
 		{
-		if (*s == accept[index])
-		{
-			bytes++;
-			break;
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[index + 1] == '\0')
+				return (bytes);
 		}
-		else if (accept[index +1] == '\0')
-			return (bytes);
-		}
-	s++;
+		s++;
 	}
-return (bytes);
+	return (bytes);
 }
