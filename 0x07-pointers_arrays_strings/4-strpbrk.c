2d45922098edca307e68b8b093d27c68d98d0f8d
get201528 <getsun201528@gmail.com>
2023-01-03 15:41:39 -0800

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strbrk - search a string for any of a set of bytes
+ * @s: The string to be searched.
+ * @accept: Teh set of bytes to be searched for .
+ * Return: if a set is matched - a pointer to the matched byte.
+ * if no set is matched - NULL
+ */
+char *_strbrk(char *s, char *accept)
+{
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+				return (s);
+		}
+		s++
+	}
+	return ('\0');
+}
