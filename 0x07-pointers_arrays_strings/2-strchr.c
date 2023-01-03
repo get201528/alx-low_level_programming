82bfbb630c477e1defb9f4faa7874422116348be
get201528 <getsun201528@gmail.com>
2023-01-03 15:08:31 -0800

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - Locate a character in a string
+ * @s:The string to be seaarched
+ * @c: Tint index;
+ * Return: if c is found -a pointer to the first occurence.
+ * if c is not found - NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+	return ('\0');
+}
