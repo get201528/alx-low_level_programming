147f6c651e29f938aab627bed682857c0ea98768
get201528 <getsun201528@gmail.com>
2023-01-03 15:24:21 -0800

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - gets the lenght of a prefix sustrings
+ * @s: The sting to be searched
+ * @accept: the prefix to e measured
+ * Return: The numer of bytes in switch
+ * consite only of byte from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int index;
+	
+	while (*s)
+	{
+		for (index = 0;  accept[index]; index++)
+		{
+		if (*s == accept[index])
+		{
+			bytes++;
+			break;
+		}
+		else if (accept[index +1] == '\0')
+			return (bytes);
+		}
+	s++;
+	}
+return (bytes);
+}
