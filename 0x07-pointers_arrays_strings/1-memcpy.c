649e3ee0c6ac421356664e34bb3060eade7aaa4e
get201528 <getsun201528@gmail.com>
2023-01-03 06:03:04 -0800

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,7 @@
  * @n: max bytes to use
  * Return: dest
  */
-char *_memcpy(char *dest char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 	for (i = 0; n > 0; i++, n--)
