8914d021144eba423b9d96221870e0dc956be960
get201528 <getsun201528@gmail.com>
2023-01-03 16:24:09 -0800

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,11 +11,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = b;
+	int index;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-
-	return (memory);
+	for (; n > 0; index++)
+	{
+		s[index] = b;
+		n--;
+	}
+	return (s);
 }
