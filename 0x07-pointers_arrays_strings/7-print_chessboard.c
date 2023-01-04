47774a5d14553a0f10f0f616520d6b1e7e9b01a6
get201528 <getsun201528@gmail.com>
2023-01-03 16:49:39 -0800

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - Entry point
+ * @a: an aray
+ * Return 0 on success
+ */
+void print_chessboard(chr(*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
